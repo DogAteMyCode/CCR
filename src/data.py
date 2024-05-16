dcd96ceb05e95e4c94288b3de276eea64f5f8c23
VoidNull <nullvoid0div0@protonmail.com>
2024-05-15 22:19:06 -0600

    added shapefile support

--- src/data.py
+++ src/data.py
@@ -5,8 +5,9 @@ import pandas as pd
 import geopandas as gpd
 import plotly.express as px
 import plotly.graph_objects as go
-
+from unidecode import unidecode
 from cleaning import CleaningMethod, DataSource
+from sources import read_data_source
 
 
 class Data:
@@ -34,8 +35,8 @@ class Data:
 
     def get_db(self, key):
         source = self.clean_sources[key]
+        db = read_data_source(source.data_source)
 
-        db = pd.read_pickle(source.data_source.data_file_location)
         cols = source.columns
         db = db.rename(columns=cols)
         db = db[cols.values()]
@@ -57,7 +58,10 @@ class Data:
 if __name__ == '__main__':
     data = Data('cleaning.json')
     print(data)
+    if not os.path.isdir('figures'):
+        os.mkdir('figures')
     # db_data: gpd.GeoDataFrame = data['factibilidad hidrica']
+    # print(db_data.geometry)
     # db_data['n_id'] = range(len(db_data))
     #
     # fig = px.choropleth_mapbox(data_frame=db_data, geojson=db_data.geometry,
@@ -69,12 +73,48 @@ if __name__ == '__main__':
     #                            hover_data=['delegacion'], title='Factibilidad hídrica en la ciudad de méxico')
     # fig.update_geos(fitbounds="locations")
     # # fig.show()
-    # if not os.path.isdir('figures'):
-    #     os.mkdir('figures')
+
     # fig.write_image('figures/factibilidad_hidrica.png', width=650, height=650, scale=15, engine='orca')
     # del db_data
 
-    db_data = data['consumo']
-    geo_data = gpd.read_file('alcaldias.json')
-    print(tuple(sorted(map(lambda x: x.lower(), db_data['alcaldia'].unique()))))
-    print(tuple(sorted(map(lambda x: x.lower(),geo_data['NOMBRE'].unique()))))
+    # db_data = data['consumo']
+    # geo_data = gpd.read_file('alcaldias.json')
+    # mapping = dict(zip(tuple(map(lambda x: unidecode(x.lower()), geo_data['NOMGEO'])), geo_data['NOMGEO']))
+    # db_data['alcaldia'] = tuple(map(lambda x: mapping[unidecode(x.lower())], db_data['alcaldia']))
+    # # for i in sorted(db_data['frequency'].unique()):
+    # month_freq = db_data.groupby('alcaldia')[['suma de consumo por colonia en m3 bimestral', 'promedio de consumo por colonia en m3 bimestral', 'promedio por numero de viviendas']].mean()
+    # geo_data.rename(columns={'NOMGEO': 'alcaldia'}, inplace=True)
+    # merge = pd.merge(geo_data, month_freq, on=['alcaldia'])
+    #
+    # merge['n_id'] = range(len(merge))
+    #
+    #
+    # fig = px.choropleth_mapbox(data_frame=merge, geojson=merge.geometry,
+    #                            locations='n_id',
+    #                            featureidkey='id',
+    #                            color='promedio por numero de viviendas',
+    #                            mapbox_style="carto-positron",
+    #                            opacity=0.5,
+    #                            center={"lat": 19.3457, "lon": -99.1318}, zoom=8.5,
+    #                            title=f'Promedio de consumo de agua en m3 bimestral por vivienda'
+    #                            )
+    #
+    # fig.write_image('figures/consumo.png', width=650, height=650, scale=15, engine='orca')
+    # del db_data
+
+    db_data = data['violencia por colonia']
+    db_data['n_id'] = range(len(db_data))
+
+    print(db_data.columns)
+
+    fig = px.choropleth_mapbox(data_frame=db_data, geojson=db_data.geometry,
+                               locations='n_id', featureidkey='id',
+                               color='marginalidad y violencia',
+                               mapbox_style="carto-positron",
+                               opacity=0.5, center={"lat": 19.3657, "lon": -99.1318}, zoom=9.0,
+                               hover_data=['alcaldia', 'colonia'], title='Violencia en la ciudad de méxico')
+    fig.update_geos(fitbounds="locations")
+    # fig.show()
+
+    fig.write_image('figures/violencia.png', width=650, height=650, scale=15, engine='orca')
+
--- src/sources.py
+++ src/sources.py
@@ -44,6 +44,70 @@ def multiline_input(prompt: str) -> list[str]:
     return out
 
 
+class DataSource(NamedTuple):
+    name: str
+    data_location: str
+    data_file_type: str
+    data_sheet_name: str | None
+    data_location_z: str | None
+    data_file_type_z: str | None
+    data_file_location: str | None = None
+
+
+def read_data_source(data_source: DataSource) -> pd.DataFrame | gpd.GeoDataFrame:
+    try:
+        db = pd.read_pickle(data_source.data_file_location)
+        return db
+    except FileNotFoundError:
+        if data_source.data_file_type == "xlsx":
+            excel_file = read_xlsx_file(data_source.data_location)
+            db = excel_file[data_source.data_sheet_name]
+            db.to_pickle(data_source.data_file_location)
+            return db
+
+        elif data_source.data_file_type == "csv":
+            db = read_csv_file(data_source.data_location)
+            db.to_pickle(data_source.data_file_location)
+            return db
+
+        elif data_source.data_file_type == "geojson":
+            db = gpd.read_file(data_source.data_location)
+            db.to_pickle(data_source.data_file_location)
+            return db
+
+        if data_source.data_file_type == "zip":
+            zip_data_ = read_zip_file(data_source.data_location)
+            file_data = io.BytesIO(zip_data_.read(data_source.data_location_z))
+
+            if data_source.data_file_type_z == "xlsx":
+                excel_file = read_xlsx_file(file_data)
+                db = excel_file[data_source.data_sheet_name]
+                db.to_pickle(data_source.data_file_location)
+                return db
+
+            elif data_source.data_file_type_z == "csv":
+                metadata = detect(zip_data_.read(data_source.data_location_z))
+                db = read_csv_file_bytes(file_data, metadata['encoding'])
+                db.to_pickle(data_source.data_file_location)
+                return db
+
+            elif data_source.data_file_type_z == "geojson":
+                if data_source.data_file_type_z.endswith('.shp'):
+                    os.mkdir('temp')
+                    tempPathP = os.path.join('temp', data_source.data_location_z)
+                    zip_data_.extractall('temp')
+                    db = gpd.read_file(tempPathP)
+                else:
+                    os.mkdir('temp')
+                    tempPathP = os.path.join('temp', data_source.data_location_z.split('/')[-1])
+
+                    with open(tempPathP, 'xb') as f:
+                        f.write(file.getbuffer().tobytes())
+                    db = gpd.read_file(tempPathP)
+                db.to_pickle(data_source.data_file_location)
+                return db
+
+
 if __name__ == "__main__":
     locations = multiline_input("Enter file url: ")
     file_types = []
@@ -55,18 +119,8 @@ if __name__ == "__main__":
             file_types.append('csv')
         elif location.endswith('.xlsx') or location.endswith('.xls') or location.endswith('xlsb'):
             file_types.append('xlsx')
-        elif location.endswith('json') or location.endswith('.geojson'):
+        elif location.endswith('json') or location.endswith('.geojson') or location.endswith('.shp'):
             file_types.append('geojson')
-
-
-    class DataSource(NamedTuple):
-        name: str
-        data_location: str
-        data_file_type: str
-        data_sheet_name: str | None
-        data_location_z: str | None
-        data_file_type_z: str | None
-
 
     data: list[DataSource] = list()
     data_frames: list[pd.DataFrame] = list()
@@ -104,10 +158,13 @@ if __name__ == "__main__":
             locations_zip = multiline_input(f"{str(files)}\nEnter locations: ")
             file_types_zip = []
             for location_z in locations_zip:
+                print(location_z)
                 if location_z.endswith('.csv'):
                     file_types_zip.append('csv')
                 elif location_z.endswith('.xlsx') or location.endswith('.xls') or location.endswith('xlsb'):
                     file_types_zip.append('xlsx')
+                elif location_z.endswith('json') or location_z.endswith('.geojson') or location_z.endswith('.shp'):
+                    file_types_zip.append('geojson')
             for location_z, file_type_z in zip(locations_zip, file_types_zip):
                 file = io.BytesIO(zip_data.read(location_z))
                 print(location_z)
@@ -132,7 +189,20 @@ if __name__ == "__main__":
 
                 elif file_type_z == "geojson":
                     name_to_use = input("Enter name to use: ")
-                    df = gpd.read_file(location_z)
+                    if location_z.endswith('.shp'):
+                        os.mkdir('temp')
+                        tempPath = os.path.join('temp', location_z)
+                        zip_data.extractall('temp')
+                        df = gpd.read_file(tempPath)
+                    else:
+                        os.mkdir('temp')
+                        tempPath = os.path.join('temp', location_z.split('/')[-1])
+
+                        with open(tempPath, 'xb') as f:
+                            f.write(file.getbuffer().tobytes())
+                        df = gpd.read_file(tempPath)
+                    import shutil
+                    shutil.rmtree('temp')
                     ds = DataSource(name_to_use, location, 'csv', None, location_z, 'geojson')
                     data_frames.append(df)
                     data.append(ds)
@@ -155,4 +225,4 @@ if __name__ == "__main__":
         temp_data = json.load(f)
 
     with open("tempdata.json", 'w', encoding='utf-8') as f:
-        json.dump(data_dict+temp_data, f)
+        json.dump(data_dict + temp_data, f)
